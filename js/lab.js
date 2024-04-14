f2c3746ea3f00f098dbfb42692e7d57456bd211a
Stephen Whitted <stephent.whitted@gmail.com>
2024-04-14 18:30:58 -0400

    Fizz buzz example-hopefully-correct

--- js/lab.js
+++ js/lab.js
@@ -1,1 +1,33 @@
 console.log("Let's start writing loops!")
+/**Part 1: Fizz Buzz 
+
+Loop through all numbers from 1 to 100.
+If a number is divisible by 3, log “Fizz.”
+If a number is divisible by 5, log “Buzz.”
+If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+If a number is not divisible by either 3 or 5, log the number.
+Remember to commit your solution once it is working.
+
+
+
+
+
+
+
+**/
+for (let i = 1; i <= 100; i++) {
+    
+    switch (true) {
+        case (i % 3 === 0 && i % 5 === 0):
+            console.log("Fizz Buzz");
+            break;
+        case (i % 3 === 0):
+            console.log("Fizz");
+            break;
+        case (i % 5 === 0):
+            console.log("Buzz");
+            break;
+        default:
+            console.log(i);
+    }
+}
