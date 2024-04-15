db86ff09cb78fe0b0198c256d953b80f75a95d9c
Stephen Whitted <stephent.whitted@gmail.com>
2024-04-14 21:14:56 -0400

    Part 2

--- js/lab.js
+++ js/lab.js
@@ -31,3 +31,76 @@ for (let i = 1; i <= 100; i++) {
             console.log(i);
     }
 }
+
+
+
+/* Part 2:
+Write a script that accomplishes the following:
+Declare an arbitrary number, n.
+Create a loop that searches for the next prime number, starting at n and incrementing from there.
+As soon as you find the prime number, log that number and exit the loop.
+Continuing with the example above, if n is equal to 4, your loop should log 5. Similarly, if n is 5, it should log 7, and if n is 9, it should log 11. Test your loop with higher numbers and reference an online prime number table to determine the accuracy of your code.
+*/
+
+let n = 4;
+
+
+let numberToFind = n + 1;
+
+let findPrime = false;
+
+
+while (!findPrime) {
+    // Assume the number is prime
+    let isPrime = true;
+
+   
+    for (let i = 2; i < numberToFind; i++) {
+        if (numberToFind % i === 0) {
+            // not prime if divisible
+            isPrime = false;
+            break; // if prime exit loop
+        }
+    }
+
+    // if not prime
+    if (isPrime) {
+        console.log(`The next prime number after ${n} is ${numberToFind}`);
+        findPrime = true; // exit the loop if true
+    } else {
+        // check the next number if not prime
+        numberToFind++;
+    }
+}
+//////////////////////////////////////////////////////////////////////////////////////
+//Let's confirm the loop by trying another n "numerical value"
+let n2 = 5;
+
+
+let numbToFind = n2 + 1;
+
+let f1ndPrime = false;
+
+
+while (!f1ndPrime) {
+    // Assume the number is prime
+    let isPrime = true;
+
+   
+    for (let i = 2; i < numbToFind; i++) {
+        if (numbToFind % i === 0) {
+            // not prime if divisible
+            isPrime = false;
+            break; // if prime exit loop
+        }
+    }
+
+    // if not prime
+    if (isPrime) {
+        console.log(`The next prime number after ${n2} is ${numbToFind}`);
+        f1ndPrime = true; // exit the loop if true
+    } else {
+        // check the next number if not prime
+        numbToFind++;
+    }
+}
