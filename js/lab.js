10764b595362a525fa6a8ee55ee71925bf2b340a
Stephen Whitted <stephent.whitted@gmail.com>
2024-04-14 23:41:59 -0400

    commit part 3b

--- js/lab.js
+++ js/lab.js
@@ -144,3 +144,28 @@ console.log(dataArray);
 
 
 // B)
+/*
+Use the example string provided above to test your program. Once you are confident it is working correctly, try the following string to see if your program works properly with other data.
+Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232
+
+
+*/
+const csvData2 = 'Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232';
+
+//                          split the CSV string into rows where '\n'
+const rows2 = csvData.split('\n');
+
+//                          allow an empty arrray to hold all the data
+let dataArray2 = [];
+
+//                           go through each row one by one
+for (let i = 0; i < rows2.length; i++) {
+    //                       use commas to split each row into columns (',')
+    const columns = rows2[i].split(',');
+    //                       add the array of columns to the main data array
+    dataArray2.push(columns);
+}
+
+//                          output the array to console
+console.log(dataArray2);
+
